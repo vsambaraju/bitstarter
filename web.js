075278ca9b5c98dabdd6b11d29ea24fe27ed0abb
vsambaraju <vsambaraju@gmail.com>
2013-07-23 07:19:40 +0000

    Updated web.js to read fromindex.html

--- web.js
+++ web.js
@@ -1,9 +1,19 @@
 var express = require('express');
+var fs = require('fs');
+var file = "index.html";
+var content;
+fs.readFileSync(file,function(err,data){
+if(err){
+    throw err;
+}
+content=buf.toString('utf8',0,data);
+});
+
 var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(content);
 });
 
 var port = process.env.PORT || 5000;
